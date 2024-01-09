728d0415120d287a20084602f9d4a7dbf013544b
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-01-10 03:10:46 +0500

    complate style of Single Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,32 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import { SinglePage } from "./page";
+import { useState } from "react";
 
 function App() {
+	const [inputValue, inputValueState] = useState("all");
+	const [selectValue, selectValueState] = useState("");
+
 	return (
 		<>
 			<Header />
 			<main className="site-main">
 				<Routes>
-					{/* <Route element={<Main />} path="/" /> */}
-					<Route element={<SinglePage />} path="/" />
+					<Route
+						element={
+							<Main
+								inputValue={inputValue}
+								inputValueState={inputValueState}
+								selectValue={selectValue}
+								selectValueState={selectValueState}
+							/>
+						}
+						path="/"
+					/>
+					<Route
+						element={<SinglePage inputValue={inputValue} selectValue={selectValue} />}
+						path={`/${selectValue || inputValue}`}
+					/>
 				</Routes>
 			</main>
 		</>
